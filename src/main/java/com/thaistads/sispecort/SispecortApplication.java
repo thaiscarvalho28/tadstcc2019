7ee21d04a0c162bc09fc26eb5f0ee40b2e761a94
Thais Carvalho <thaiscarvalho2898@gmail.com>
2019-08-08 20:04:04 -0300

    Projeto criado

---
+++ src/main/java/com/thaistads/sispecort/SispecortApplication.java
@@ -0,0 +1,13 @@
+package com.thaistads.sispecort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispecortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispecortApplication.class, args);
+	}
+
+}
